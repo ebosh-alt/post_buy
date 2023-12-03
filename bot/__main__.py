0f3d905b5aebef7bda055b86da94680bee0fc313
Nikita I <isakovn2005@gmail.com>
2023-12-03 09:33:08 +0300

    debug

--- bot/__main__.py
+++ bot/__main__.py
@@ -24,7 +24,7 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         filemode="w",
-                        # filename="logging.log",
+                        filename="logging.log",
                         format="%(levelname)s %(asctime)s %(message)s",
                         encoding='utf-8')
 
