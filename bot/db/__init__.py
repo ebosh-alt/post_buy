b74d3373f1ad032d69e1d6e18d2e667b1e939500
Nikita I <isakovn2005@gmail.com>
2023-12-12 12:38:07 +0300

    add send post to support

--- bot/db/__init__.py
+++ bot/db/__init__.py
@@ -3,7 +3,7 @@ from .Channels import Channel, Channels
 from .Prices import Price, Prices
 from .Publications import Publication, Publications
 
-db_file_name = "D:/telegram_bots/post_buy/bot/db/database"
+db_file_name = "bot/db/database"
 users = Users(db_file_name=db_file_name, table_name="users")
 channels = Channels(db_file_name=db_file_name, table_name="channel")
 prices = Prices(db_file_name=db_file_name, table_name="price")
